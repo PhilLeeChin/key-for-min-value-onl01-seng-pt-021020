d321e10c37bca9bfab1b287f1ccfa326c27b1839
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-20 02:40:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     m_val = name_hash.f[1]
     m_name = name_hash.f[0]
     name_hash.each do |name, x|
-      if x < m_val
+      if m_val < x
         m_val = x
         m_name = name
       end
